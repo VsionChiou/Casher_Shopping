7c31950c4252e1901b82689fada972625c29dc9f
unknown <qms42621@qq.com>
2018-07-24 17:18:58 +0800

    second

--- shopping.py
+++ shopping.py
@@ -1,44 +1,31 @@
-Goods = ['饮料','矿泉水','面包','可乐','冰淇凌']
-His=[];
+Goods = {'饮料': 3, '矿泉水': 1, '面包': 4, '可乐': 3.5, '冰淇凌': 5}
+goodsNow = [];
+His = [];
 
 
 salary = int(input("请输入你的工资："))
+i=1
+for goods in Goods.items():
+    print(i,goods)
+    goodsNow.append(goods);
+    i +=1;
 
-for i in range(len(Goods)):
-    print(i+1,Goods[i])
-
-Salary=0
-
-while Salary<salary:
-
-    BuyItem=int(input("请输入你要购买的商品编号："))
-    if BuyItem==1:
-        salary=salary-5;
-        His.append("饮料");
-    elif BuyItem==2:
-        salary=salary-1;
-        His.append("矿泉水");
-    elif BuyItem==3:
-        salary=salary-3;
-        His.append("面包");
-    elif BuyItem==4:
-        salary=salary-4;
-        His.append("可乐");
-    elif BuyItem==5:
-        salary=salary-5;
-        His.append("冰激凌");
-    elif BuyItem>5:
-        print("请输入正确的编号！");
-    elif BuyItem==0:
-        break;
-if salary>=0 :
-    print("购买的商品：",",".join(His[:]),"余额：",salary);
+Salary = 0
+while Salary < salary:
+    Item = int(input("请输入你要购买的商品编号："))
+    if Item >0:
+        His.append(goodsNow[Item-1])
+        salary=salary-goodsNow[Item-1][1];
+    if Item==0:
+        break
+if salary>=0:
+    pass
 elif salary<0:
-    del His[-1]
-    print("购买的商品：",",".join(His[:]));
-    print("余额不足，请充值。。。")
+    salary = salary + His[-1][1]
+    print('您的余额不足！')
+    His.pop(-1)
 
 
-
-
+print("您购买的商品如下：",His)
+print("您的余额还剩：%d元" % salary)
 
