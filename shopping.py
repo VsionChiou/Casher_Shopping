e566d1d2729791e08ad916c372dd554ac2ac1f75
unknown <qms42621@qq.com>
2018-08-29 18:45:08 +0800

    Version 4.0

--- shopping.py
+++ shopping.py
@@ -3,82 +3,114 @@ goodsNow = [];
 His = [];
 
 
-def register():
-    loginlog = open("cache/login.log", 'a')
-    id = int(input("请输入你的自定义ID（限6位）："))
-    pswd = int(input("请输入密码(仅数字！)："))
-    loginlog.write("%s\n" % id)
-    loginlog.write("%s\n\n" % pswd)
-    loginlog.close()
-    print("注册成功！")
+class Account(object):
 
-def login():
-    loginlog=open('cache/login.log','r')
-    id = input("请输入你的自定义ID（限6位）：")
-    pswd = int(input("请输入密码："))
-    for line in loginlog:
-        if id in line:
-            f_password=loginlog.readline()
-            password=int(f_password)
+    # loginlog = open("cache/login.log")
 
-            if pswd==password:
-                print("登录成功！")
-                System()
-            else :
-                print("密码有误，请重新输入。")
-                login()
-        else:
-            print("无此ID，请注册。。。")
-            register()
+    def __init__(self,id,pswd,salary):
+        self.id = id
+        self.pswd = pswd
+        self.salary = salary
 
-    loginlog.close()
+    def register(self):
+        loginlog = open("cache/login.log", 'a')
+        # id = int(input("请输入你的自定义ID（限6位）："))
+        # pswd = int(input("请输入密码(仅数字！)："))
+        loginlog.write("%s\n" % self.id)
+        loginlog.write("%s\n\n" % self.pswd)
 
-def System():
-    salary = int(input("请输入你的工资："))
-    i = 1
-    for goods in Goods.items():
-        print(i, goods)
-        goodsNow.append(goods);
-        i += 1;
+        loginlog.close()
+        print("注册成功！")
+        # return loginlog
 
-    Salary = 0
-    while Salary < salary:
-        Item = int(input("请输入你要购买的商品编号："))
-        if Item > 0:
-            His.append(goodsNow[Item - 1])
-            salary = salary - goodsNow[Item - 1][1];
-        if Item == 0:
-            break
-    if salary >= 0:
-        pass
-    elif salary < 0:
-        salary = salary + His[-1][1]
-        print('您的余额不足！')
-        His.pop(-1)
+    def login(self):
 
-    print("您购买的商品如下：", His)
-    print("您的余额还剩：%d元" % salary)
+        loginlog = open('cache/login.log','r')
+        # id = input("请输入你的自定义ID（限6位）：")
+        # pswd = int(input("请输入密码："))
+        for line in loginlog:
+            if self.id in line:
+                f_password=loginlog.readline()
+                password=int(f_password)
 
-main=int(input('请输入操作：1.登录，2.注册'))
+                if self.pswd==password:
+                    print("登录成功！")
 
-if main==1:
-    login()
+                    mysystem = system()
+                    mysystem.System()
+                    break
+                else :
+                    print("密码有误，请重新输入。")
+                    self.login()
+
+            '''
+            else:
+                print("无此ID，请注册。。。")
+                self.register()
+                '''
+        loginlog.close()
+        # return loginlog
 
 
+    def Balance(self):
+        return self.salary
 
-if main==2:
-    register()
+
+class system(object):
+
+    def System(self):
+        # salary = int(input("请输入你的工资："))
+        # Account.salary
+
+        i = 1
+        for goods in Goods.items():
+            print(i, goods)
+            goodsNow.append(goods);
+            i += 1;
+
+        Salary = 0
+        while Salary < myaccount.salary:
+            Item = int(input("请输入你要购买的商品编号："))
+            if Item > 0:
+                His.append(goodsNow[Item - 1])
+                myaccount.salary = myaccount.salary - goodsNow[Item - 1][1];
+            if Item == 0:
+                break
+        if myaccount.salary >= 0:
+            pass
+        elif myaccount.salary < 0:
+            myaccount.salary = myaccount.salary + His[-1][1]
+            print('您的余额不足！')
+            His.pop(-1)
+
+        print("您购买的商品如下：", His)
+        print("您的余额还剩：%d元" % myaccount.salary)
+
+
+main = int(input('请输入操作：1.登录，2.注册'))
+
+id = input("请输入你的自定义ID（限6位）：")
+pswd = int(input("请输入密码："))
+salary = int(input("请输入你的工资："))
+
+myaccount = Account(id, pswd, salary)
+
+if main == 1:
+    myaccount.login()
+
+
+if main == 2:
+    myaccount.register()
 
     while(1):
-        choose=input("是否要登录呢？ y/n")
-        if choose=='y' or 'yes':
-            login()
-        elif choose=='n' or 'no':
+        choose = input("是否要登录呢？ y/n")
+
+        if choose == "y" and "yes":
+            myaccount.login()
+        elif choose == "n" and "no":
             exit()
         else:
             print("输入错误！")
-
-
 
 
 
